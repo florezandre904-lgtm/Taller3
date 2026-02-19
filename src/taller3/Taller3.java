a436af7c8aad73fbcb2b03119f70380ffaa9251f
Andrea <florezandre904.com>
2026-02-18 21:28:12 -0500

    finalizado

--- src/taller3/Taller3.java
+++ src/taller3/Taller3.java
@@ -16,7 +16,7 @@ public class Taller3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ejercicio2();
+        ejercicio4();
     }
     public static void ejercicio1() {
         
@@ -78,23 +78,140 @@ public class Taller3 {
         }
     }
     public static void ejercicio3 () {
-        int menu = Integer.parseInt(JOptionPane.showInputDialog("ingrese la opcion que desea realizar:\n"
-                + "1. registrar notas.\n"
-                + "2. calcular promedio por materia.\n"
-                + "3. calcular promedio total del curso.\n"
-                + "4. mostrar estudiantes aprobados y reprobados.\n"
-                + "5. salir.\n"));
-        while (true) {            
-            switch (menu) {
-                case 1:
-                    
-                    
+        String nombres[] = new String[50];
+    float matnotas[] = new float[50];
+    float ingnotas[] = new float[50];
+    float espnotas[] = new float[50];
+
+    int cantidad = 0;
+
+    while (true) {
+        int menu = Integer.parseInt(JOptionPane.showInputDialog(
+                "   MENU:\n"
+                + "1. Registrar notas.\n"
+                + "2. Calcular promedio por materia.\n"
+                + "3. Calcular promedio total del curso.\n"
+                + "4. Mostrar estudiantes aprobados y reprobados.\n"
+                + "5. SALIR.\n"));
+
+        switch (menu) {
+            case 1:
+                String nombre = JOptionPane.showInputDialog("ingrese el nombre del estudiante:");
+                nombres[cantidad] = nombre;
+                float matnota = Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota de Matemáticas:"));
+                matnotas[cantidad] = matnota;
+                float ingnota = Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota de  Inglés:"));
+                ingnotas[cantidad] = ingnota;
+                float espnota = Float.parseFloat(JOptionPane.showInputDialog("ingrese la nota de Español:"));
+                espnotas[cantidad] = espnota;
+
+                cantidad++;
+                break;
+
+            case 2:
+                float sumaMat = 0, sumaIng = 0, sumaEsp = 0;
+                for (int i = 0; i < cantidad; i++) {
+                    sumaMat += matnotas[i];
+                    sumaIng += ingnotas[i];
+                    sumaEsp += espnotas[i];
+                }
+
+                JOptionPane.showMessageDialog(null,"el promedio de Matemáticas es: " + (sumaMat / cantidad) +
+                        "\n el promedio de Inglés es: " + (sumaIng / cantidad) +
+                        "\n el promedio de Español es: " + (sumaEsp / cantidad));
+                break;
+
+            case 3:
+                float sumaTotal = 0;
+                for (int i = 0; i < cantidad; i++) {
+                    float promedioEst = (matnotas[i] + ingnotas[i] + espnotas[i]) / 3;
+                    sumaTotal += promedioEst;
+                }
+                JOptionPane.showMessageDialog(null,"el promedio total del curso es: " + (sumaTotal / cantidad));
+                break;
+
+            case 4:
+                String aprobados = "";
+                String reprobados = "";
+                for (int i = 0; i < cantidad; i++) {
+                    float promedio = (matnotas[i] + ingnotas[i] + espnotas[i]) / 3;
+                while (promedio >= 3.0) {
+                    aprobados += nombres[i] + " Promedio: " + promedio + "\n";
                     break;
+                }
+                while (promedio < 3.0) {
+                    reprobados += nombres[i] + " Promedio: " + promedio + "\n";
+                    break;
+                }
+                }
+                JOptionPane.showMessageDialog(null,
+                        "APROBADOS:\n" + aprobados +
+                        "\nREPROBADOS:\n" + reprobados);
+                break;
+
+            case 5:
+                System.exit(0);
+        }
+    }}
+    public static void ejercicio4() {
+        int n = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas personas desea ingresar?"));
+
+        String nombres[] = new String[n];
+        char sexos[]= new char[n];
+
+        int contadorH = 0;
+        int contadorM = 0;
+
+        for (int i = 0; i < n; i++) {
+            nombres[i] = JOptionPane.showInputDialog("Nombre de la persona " + (i + 1) + ":");
+            
+            boolean valido = false;
+            while (!valido) {
+                String s = JOptionPane.showInputDialog("Sexo de " + nombres[i] + " (f/m):").toLowerCase();
                 
+                if (!s.isEmpty()) {
+                    char letra = s.charAt(0);
+                    switch (letra) {
+                        case 'm':
+                            sexos[i] = 'm';
+                            contadorH++; 
+                            valido = true;
+                            break;
+                        case 'f':
+                            sexos[i] = 'f';
+                            contadorM++; 
+                            valido = true;
+                            break;
+                        default:
+                            JOptionPane.showMessageDialog(null, "Error: Use 'f' o 'm'");
+                    }
+                }
             }
         }
-        
-        
-        
+        String[] listaHombres = new String[contadorH];
+        String[] listaMujeres = new String[contadorM];
+
+        int idxH = 0;
+        int idxM = 0; 
+
+        for (int i = 0; i < n; i++) {
+            switch (sexos[i]) {
+                case 'm':
+                    listaHombres[idxH] = nombres[i];
+                    idxH++;
+                    break;
+                case 'f':
+                    listaMujeres[idxM] = nombres[i];
+                    idxM++;
+                    break;
+            }
+        }
+        String resH = "Hombres: ";
+        for (String h : listaHombres) resH += h + " ";
+
+        String resM = "Mujeres: ";
+        for (String m : listaMujeres) resM += m + " ";
+
+        JOptionPane.showMessageDialog(null, resH + "\n" + resM);
     }
 }
